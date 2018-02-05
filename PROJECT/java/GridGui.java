9a6b6d079de3297969dd6c76d419e81614ea3831
Bartosz Kosakowski <kosakowb@mcmaster.ca>
2018-02-04 19:07:24 -0500

    added start and end buttons so that the user can control when the game starts and ends

--- PROJECT/java/GridGui.java
+++ PROJECT/java/GridGui.java
@@ -3,18 +3,50 @@
 * Draws the grid used to display Conway's Game of Life
 */
 import javax.swing.JFrame;
-import java.awt.*;
+
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class GridGui extends JFrame{
 	private static GridGui mygrid = new GridGui();
 	
+	private static JButton startButton = new JButton("Start");
+	private static StartGame startAction = new StartGame();
+	private static JButton endButton = new JButton("End");
+	private static EndGame endAction = new EndGame();
+	
+	private static void setUpButtons(){
+		startButton.addActionListener(startAction);
+		mygrid.add(startButton);
+		endButton.addActionListener(endAction);
+		mygrid.add(endButton);
+	}
+	
 	public static void drawGrid(){
+		mygrid.setLayout(new FlowLayout());
 		mygrid.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mygrid.setSize(200,200);
+		mygrid.setSize(300,300);
 		mygrid.setVisible(true);
 		mygrid.setTitle("Conway's Game of Life");
+		
+		setUpButtons();
 	}
-}
+}//end of GridGui
+
+class StartGame implements ActionListener{
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		ConwaysGame.startGame();
+	}
+}//end of StartGame
+
+class EndGame implements ActionListener{
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		ConwaysGame.endGame();
+		System.out.println("DONE");
+	}
+}//end of EndGame
