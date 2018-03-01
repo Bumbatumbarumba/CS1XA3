ab5ace2f4ba15a88139f0f4bbe76ec9cee98db53
Bartosz Kosakowski <kosakowb@mcmaster.ca>
2018-03-01 00:08:22 -0500

    100% complete, however I will add some bells and whistles to make it cooler

--- PROJECT/java/ConwaysGame.java
+++ PROJECT/java/ConwaysGame.java
@@ -1,51 +1,88 @@
 import java.awt.Color;
-import javax.swing.JPanel;
+import java.util.Arrays;
+import java.util.Collections;
 
 /*Created by Bartosz Kosakowski
 * 01/02/2018 (dd/mm/yyyy)
 * Coway's Game of Life in Java
 */
 public class ConwaysGame {
-	private static int rows = 20;
-	private static int columns = 20;
+	private static int rows = 100;
+	private static int columns = 100;
 	private static Cell[][] grid = new Cell[rows][columns];
 	private static boolean runGame = false;
+	private static Integer[] randRows = new Integer[rows];
+	private static Integer[] randCols = new Integer[columns];
+	private static int populationSeedType = 0;
+	private static boolean makingPopChoice = true;
 
 	// change this to be a double between 0 and 1 to determine how many live
-	// members there will be off the bat when populate() is ran
+	// the odds of a cell starting off as alive or dead - default is 0.05
 	public static double livingLikelihood = 0.05;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		setUpRNG();
+
+		// comment and uncomment each of the following to use
+		// different seeds:
 		populate();
-		GridGui.drawGrid(grid, rows, columns);
+		// populateSquare();
+		// randSquarePop();
+		// doNotPop();
+
+		GridGui.drawUI(grid, rows, columns);
 		runGame();
-	}
+	}// end of main
 
-	public static void runGame() {
-		// while runGame is false, it will put this thread to sleep
-		// so that it only starts once the user starts the game
-		while (!runGame) {
-			try {
+	public static void runGame() throws InterruptedException {
+		while (true) {
+			// while runGame is false, it will put this thread to sleep
+			// so that it only starts once the user starts the game
+			while (!runGame) {
 				Thread.sleep(200);
-			} catch (InterruptedException e) {
-				// exception caught!
 			}
-		}
+			// game starts when runGame is true, duh
+			while (runGame) {
+				// slows down the simulation speed
+				Thread.sleep(100);
 
-		// game starts when runGame is true, duh
-		while (runGame) {
-			// iterates through the grid and
-			for (int i = 0; i < rows; i++) {
-				for (int j = 0; j < columns; j++) {
-					if (ruleCheck(i, j)) {
-						grid[i][j].makeAlive();
-					} else {
-						grid[i][j].makeDead();
+				// iterates through the grid randomly (does not pick duplicate
+				// locations)
+				// rather than linearly
+				for (int i = 0; i < rows; i++) {
+					for (int j = 0; j < columns; j++) {
+						if (ruleCheck(randRows[i], randCols[j])) {
+							grid[randRows[i]][randCols[j]].makeAlive();
+						} else {
+							grid[randRows[i]][randCols[j]].makeDead();
+						}
 					}
 				}
+
+				// checks each space linearly
+				/*
+				 * for (int i = 0; i < rows; i++) { for (int j = 0; j < columns;
+				 * j++) { if (ruleCheck(i, j)) { grid[i][j].makeAlive(); } else
+				 * { grid[i][j].makeDead(); } } }
+				 */
+				GridGui.updateGrid();
 			}
 		}
 	}// end of runGame
+
+	// sets up randRows and randCols to be a randomly ordered list of
+	// ints between 0 and rows, and 0 and columns, respectively
+	public static void setUpRNG() {
+		for (int i = 0; i < rows; i++) {
+			randRows[i] = i;
+		}
+		for (int j = 0; j < columns; j++) {
+			randCols[j] = j;
+		}
+
+		Collections.shuffle(Arrays.asList(randRows));
+		Collections.shuffle(Arrays.asList(randCols));
+	}// end of setUpRNG
 
 	// randomly populates the grid with live cells
 	public static void populate() {
@@ -59,8 +96,53 @@ public class ConwaysGame {
 		}
 	}// end of populate
 
+	// populates the grid with a square live cells placed in the middle
+	public static void populateSquare() {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				grid[i][j] = new Cell();
+			}
+		}
+		int p1 = rows / 2;
+		int p2 = columns / 2;
+
+		grid[p1][p2].makeAlive();
+		grid[p1 + 1][p2 + 1].makeAlive();
+		grid[p1 + 1][p2].makeAlive();
+		grid[p1][p2 + 1].makeAlive();
+	}// end of populateSquare
+
+	// populates the grid with random squares of cells at half of the
+	// specified likelihood
+	public static void randSquarePop() {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				grid[i][j] = new Cell();
+			}
+		}
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (Math.random() < livingLikelihood / 2) {
+					grid[i][j].makeAlive();
+					grid[Math.floorMod((i + 1), rows)][Math.floorMod(j + 1, columns)].makeAlive();
+					grid[Math.floorMod((i + 1), rows)][Math.floorMod(j, columns)].makeAlive();
+					grid[Math.floorMod(i, rows)][Math.floorMod((j + 1), columns)].makeAlive();
+				}
+			}
+		}
+	}// end of randSquarePop
+
+	// creates an area of dead cells; this was for testing
+	public static void doNotPop() {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				grid[i][j] = new Cell();
+			}
+		}
+	}// end of doNotPop
+
 	// used to count the number of live cells at any given instance
-	// not useful outside of just checking stuff
+	// not useful outside of testing
 	public static int countLive() {
 		int numLiveCells = 0;
 		for (int i = 0; i < rows; i++) {
@@ -106,47 +188,58 @@ public class ConwaysGame {
 			neighbourCounter++;
 
 		// any cell with fewer than two live neighbours dies, as
-		// if caused by underpopulation
+		// if caused by underpopulation (if alive)
 		if (neighbourCounter < 2) {
-			willSurvive = false;
+			if (grid[i][j].isLive())
+				willSurvive = false;
 		}
 		// any live cell with more than three live neighbours dies,
-		// as if by overpopulation
+		// as if by overpopulation (if alive)
 		else if (neighbourCounter > 3) {
-			willSurvive = false;
+			if (grid[i][j].isLive())
+				willSurvive = false;
 		}
 		// any dead cell with exactly three live neighbours becomes
-		// a live cell, as if by reproduction
+		// a live cell, as if by reproduction (if dead)
 		else if (neighbourCounter == 3) {
-			willSurvive = true;
+			if (!grid[i][j].isLive())
+				willSurvive = true;
 		}
 		// any live cell with two or three live neighbours lives on
-		// to the next generation
+		// to the next generation (if alive)
 		else {
-			willSurvive = true;
+			if (grid[i][j].isLive())
+				willSurvive = true;
 		}
 
 		return willSurvive;
-	}
+	}// end of ruleCheck
 
 	// changes runGame to be true
 	public static void startGame() {
 		runGame = true;
-	}
+	}// end of startGame
 
 	// changes runGame to be false
 	public static void endGame() {
 		runGame = false;
-	}
+	}// end of endGame
+
+	public static void setPopType(int val) {
+		populationSeedType = val;
+	}// end of setPopType
+
+	public static void decisionMade() {
+		makingPopChoice = false;
+	}// end of setPopType
 }// end of ConwaysGame
 
-@SuppressWarnings("serial")
-// represents a cell, with vars for position, colour and
-// whether it is alive or not
-class Cell extends JPanel{
+// represents a cell, with vars for colour and whether it is alive or not
+class Cell {
 	private Color cellColour = Color.WHITE;
 	private boolean islive = false;
 
+	// these are all pretty intuitive
 	public boolean isLive() {
 		return this.islive;
 	}
--- PROJECT/java/GridGui.java
+++ PROJECT/java/GridGui.java
@@ -13,26 +13,32 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class GridGui extends JPanel {
 
-	private static JButton startButton = new JButton("Start");
+	public static JButton startButton = new JButton("Start");
 	private static StartGame startAction = new StartGame();
-	private static JButton endButton = new JButton("End");
+	public static JButton endButton = new JButton("Pause");
 	private static EndGame endAction = new EndGame();
-
+	private static JButton randPop = new JButton("Randomly Populate");
+	private static JButton squarePop = new JButton("Centered Square Population");
+	private static JButton randSquare = new JButton("Random Square Populations");
+	private static JButton noPop = new JButton("Empty Grid");
 	private static JFrame myFrame = new JFrame();
-	
 	private static Grid g1;
-	
+
 	// adds the buttons to the frame and positions them
 	private static void setUpButtons() {
 		startButton.addActionListener(startAction);
 		startButton.setBounds(275, 600, 100, 30);
+		startButton.setVisible(true);
+
 		endButton.addActionListener(endAction);
-		endButton.setBounds(400, 600, 100, 30);
+		endButton.setBounds(275, 600, 100, 30); // 400, 600, 100, 30
+		endButton.setVisible(false);
+
 		myFrame.add(startButton);
 		myFrame.add(endButton);
 	}// end of setUpButtons
-	
-	public static void drawGrid(Cell[][] cells, int rows, int columns) {
+
+	public static void drawUI(Cell[][] cells, int rows, int columns) {
 		// myFrame.setLayout(new FlowLayout());
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setSize(750, 750);
@@ -45,12 +51,22 @@ public class GridGui extends JPanel {
 		// we add the grid to the window after setting up the buttons
 		g1 = new Grid(cells, rows, columns);
 		myFrame.add(g1);
-	}// end of drawGrid
+	}// end of drawUI
+
+	// updates the grid based on current cell statuses
+	public static void updateGrid() {
+		myFrame.repaint();
+	}// end of updateGrid
 }// end of GridGui
 
+// The following actionlistener classes allow for each button to
+// perform a particular function
 class StartGame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		GridGui.startButton.setVisible(false);
+		GridGui.endButton.setVisible(true);
+		System.out.println("START");
 		ConwaysGame.startGame();
 	}
 }// end of StartGame
@@ -58,31 +74,63 @@ class StartGame implements ActionListener {
 class EndGame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		ConwaysGame.endGame();
+		GridGui.startButton.setVisible(true);
+		GridGui.endButton.setVisible(false);
 		System.out.println("DONE");
+		ConwaysGame.endGame();
 	}
 }// end of EndGame
 
+class RandomPop implements ActionListener {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		ConwaysGame.setPopType(0);
+		ConwaysGame.decisionMade();
+	}
+}// end of EndGame
 
-//testing stuff with drawing JPanel shapes
+class SquarePop implements ActionListener {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		ConwaysGame.setPopType(1);
+		ConwaysGame.decisionMade();
+	}
+}// end of EndGame
+
+class RandSquarePop implements ActionListener {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		ConwaysGame.setPopType(2);
+		ConwaysGame.decisionMade();
+	}
+}// end of EndGame
+
+class NoPop implements ActionListener {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		ConwaysGame.setPopType(3);
+		ConwaysGame.decisionMade();
+	}
+}// end of EndGame
+
+// used to paint the grid shown on the frame
 @SuppressWarnings("serial")
 class Grid extends JPanel {
 	private int rows;
 	private int columns;
 	private Cell[][] cells;
-	
-	public Grid(Cell[][] cells, int rows, int columns){
+
+	public Grid(Cell[][] cells, int rows, int columns) {
 		this.cells = cells;
 		this.rows = rows;
 		this.columns = columns;
 	}
-	
+
 	public void paintComponent(Graphics g) {
-		for (int i = 0; i < rows; i++){
-			for (int j = 0; j < columns; j++){
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
 				g.setColor(this.cells[i][j].getColor());
-				g.fillRect(i*5, j*5, 5, 5);
-				System.out.println("cell location: ("+ i + ", " + j + ")\n" + "cell colour: " + cells[i][j].getColor());
+				g.fillRect(i * 5 + 50, j * 5 + 50, 5, 5);
 			}
 		}
 	}
